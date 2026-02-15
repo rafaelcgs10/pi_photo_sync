c4b5c3abde487542396f462a4958f1cab347ac19
rafaelcgs10 <rafaelcgs10@gmail.com>
2026-02-15 15:41:26 +0100

    Improve darktable script

--- darktable.sh
+++ darktable.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set +e
 
 shopt -s extglob
 DIRECTORY=/rafael_mounts/photos_from_rpi
@@ -6,17 +7,22 @@ STYLES=("astia")
 
 cd $DIRECTORY
 
-for folder in *; do
-    cd ${folder} 
-    for style in $STYLES; do
-        mkdir -p ${style} ;
-        raws=$(ls *.@(CR3|cr3) | sed -e 's/\.CR3$//' | sed -e 's/\.cr3$//' | sort)
-        jpgs=$(ls ${style} | sed -e 's/\.jpg$//' | sort)
-        dff=$(diff <(echo "$raws") <(echo "$jpgs") | grep "^<" | sed 's/^< //')
-        for photo in $dff; do
-            echo ${folder} ${style} ${photo} ;
-            darktable-cli ${photo}.CR3 ${style}/${photo}.jpg --style ${style} --style-overwrite ;
+while :
+do
+    for folder in *; do
+        cd ${folder} 
+        for style in $STYLES; do
+            mkdir -p ${style} ;
+            pwd
+            raws=$(ls *.@(CR3|cr3) | sed -e 's/\.CR3$//' | sed -e 's/\.cr3$//' | sort)
+            jpgs=$(ls ${style} | sed -e 's/\.jpg$//' | sort)
+            dff=$(diff <(echo "$raws") <(echo "$jpgs") | grep "^<" | sed 's/^< //')
+            for photo in $dff; do
+                echo ${folder} ${style} ${photo} ;
+                \time -f "%e" darktable-cli ${photo}.CR3 ${style}/${photo}.jpg --style ${style} --style-overwrite ;
+            done
         done
+        cd ..
     done
-    cd ..
+    sleep 3
 done
